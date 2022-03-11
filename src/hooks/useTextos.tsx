bb4af2f075753b933f11b44f40f8c4d8612d1c68
Leonardo Leite <leo.leite@hotmail.com>
2022-03-11 08:35:26 -0300

    .

--- src/hooks/useTextos.tsx
+++ src/hooks/useTextos.tsx
@@ -2,20 +2,22 @@ import { useState, useEffect } from 'react';
 
 import textos from '../mocks/textos.json';
 
-let text = {
-  boasVindas: '',
-  legenda: '',
-  legendaMelhoresProdutores: '',
-  tituloProdutores: '',
-  tituloProdutor: '',
-  tituloCestas: '',
-  topoCesta: '',
-  botaoComprar: '',
-  tituloItens: '',
-};
+let text: Array<object> = [
+  {
+    boasVindas: '',
+    legenda: '',
+    legendaMelhoresProdutores: '',
+    tituloProdutores: '',
+    tituloProdutor: '',
+    tituloCestas: '',
+    topoCesta: '',
+    botaoComprar: '',
+    tituloItens: '',
+  },
+];
 
 export default function useTextos() {
-  const [textos, setTextos] = useState(text);
+  const [textos, setTextos] = useState<Array<object>>(text);
 
   useEffect(() => {
     const retorno = textos;
